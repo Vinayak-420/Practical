b6cc4897d13d885354618061a56cc9289b3d523b
Vinayak Avasekar <112852709+Vinayak-420@users.noreply.github.com>
2023-03-17 12:05:32 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+class Ifelse
+{
+public static void main(String args[])
+{
+int x=5;
+int y=3;
+if(x+y<7)
+{
+System.out.println("x+y is less than 10 ");
+}
+System.out.println("x+y is greater than 10 ");
+}
+}
