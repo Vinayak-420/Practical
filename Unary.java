f0ef383b0e95ab6bd9ecf033c9b095b70f3e5cc9
Vinayak Avasekar <112852709+Vinayak-420@users.noreply.github.com>
2023-03-17 10:33:59 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,22 @@
+class Unary
+{
+public static void main(String args[])
+{
+int a = 5;
+int b = 6;
+System.out.println(a++);
+System.out.println(++a);
+System.out.println(a--);
+System.out.println(--a);
+
+System.out.println(a++ + ++b);
+System.out.println(++a + a++);
+System.out.println(b++ + ++a);
+
+
+System.out.println(a-- + --b);
+System.out.println(--a + a--);
+System.out.println(b-- + --a); 
+
+}
+}
