f0bfb686c1f37199c5c558f4295b867528cca20a
Vinayak Avasekar <112852709+Vinayak-420@users.noreply.github.com>
2023-03-18 19:47:11 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,20 @@
+class Switch
+{
+public static void main(String args[])
+{
+int a=1;
+
+switch(a)
+{
+case 0:
+System.out.println("number is 0");
+
+case 1:
+System.out.println("number is 1");
+
+
+default:
+System.out.println(0);
+}
+}
+}
